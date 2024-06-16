fa5ff6aa670e62529334dbd9c7bd75e288a08072
Mathew <mateusz.lu123@gmail.com>
2024-06-16 20:46:35 +0200

    fixed listing to certain port

--- src/server/wssDevServer.ts
+++ src/server/wssDevServer.ts
@@ -2,12 +2,17 @@ import { createContext } from "./context";
 import { applyWSSHandler } from "@trpc/server/adapters/ws";
 import { WebSocketServer } from "ws";
 import { appRouter } from "./api/root";
-import { pullNodeTypescript } from "./api/dockerpuller";
+
+const PORT = process.env.PORT? parseInt(process.env.PORT) : 3001; // Use the provided PORT or default to 3001 if not set
 
 const wss = new WebSocketServer({
-  port: 3001,
+  port: PORT,
 });
 const handler = applyWSSHandler({ wss, router: appRouter, createContext });
+
+wss.on("listening", () => {
+  console.log(`✅ WebSocket Server listening on ws://localhost:${PORT}`);
+});
 
 wss.on("connection", (ws) => {
   console.log(`➕➕ Connection (${wss.clients.size})`);
@@ -15,7 +20,6 @@ wss.on("connection", (ws) => {
     console.log(`➖➖ Connection (${wss.clients.size})`);
   });
 });
-console.log("✅ WebSocket Server listening on ws://localhost:3001");
 
 process.on("SIGTERM", () => {
   console.log("SIGTERM");
