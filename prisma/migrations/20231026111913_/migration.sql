1d96ac64a470be55c55cfacf3b5044c82af1b54a
Mathew <mateusz.lu123@gmail.com>
2024-06-26 14:14:46 +0200

    changed database provider

--- prisma/migrations/20231026111913_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name,folderId,projectId]` on the table `File` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "File_name_folderId_projectId_key" ON "File"("name", "folderId", "projectId");
