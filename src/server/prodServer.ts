e852eb11ae3d05e116a7c047f6fc78ac8c283009
Mathew <mateusz.lu123@gmail.com>
2024-06-28 16:08:47 +0200

    fixed server in production

--- src/server/prodServer.ts
+++ src/server/prodServer.ts
@@ -8,6 +8,8 @@ import { WebSocketServer } from "ws";
 import { env } from "../env";
 
 const port = parseInt(process.env.PORT || "3000");
+console.log({b: env.GOOGLE_CLIENT_ID})
+console.log({a: env.GOOGLE_CLIENT_SECRET})
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -16,25 +18,27 @@ void app.prepare().then(() => {
   const server = http.createServer((req, res) => {
     const proto = req.headers["x-forwarded-proto"];
     if (proto && proto === "http") {
-      // Redirect to SSL
+      // redirect to ssl
       res.writeHead(303, {
-        location: `https://${req.headers.host}${req.url}`,
+        location: `https://` + req.headers.host + (req.headers.url ?? ""),
       });
       res.end();
       return;
     }
-
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const parsedUrl = parse(req.url!, true);
+
     void handle(req, res, parsedUrl);
   });
 
-  const wssPort = process.env.WS_PORT ? parseInt(process.env.WS_PORT) : 3001;
-  const wss = new WebSocketServer({ server, path: "/ws" });
-
-  wss.on("listening", () => {
-    console.log(`✅ WebSocket Server listening on ws://localhost:${wssPort}`);
+  const wss = new WebSocketServer({
+    port: 3001,
   });
 
+  wss.on("listening", () => {
+    console.log(`✅ WebSocket Server listening on ws://localhost:${3001}`);
+  });
+  
   wss.on("connection", (ws) => {
     console.log(`➕➕ Connection (${wss.clients.size})`);
     ws.once("close", () => {
@@ -44,17 +48,17 @@ void app.prepare().then(() => {
 
   const handler = applyWSSHandler({ wss, router: appRouter, createContext });
 
+
   process.on("SIGTERM", () => {
     console.log("SIGTERM");
     handler.broadcastReconnectNotification();
     wss.close();
   });
+  server.listen(port);
 
-  server.listen(port, () => {
-    console.log(
-      `> Server listening at http://localhost:${port} as ${
-        dev ? "development" : process.env.NODE_ENV
-      }`
-    );
-  });
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`,
+  );
 });
